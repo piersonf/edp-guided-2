ca576c7b13b55563141506e7453c3399a95af9fe
piersonf <piersonfaulk@gmail.com>
2025-06-30 14:50:37 -0400

    working server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,28 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import pg from 'pg';
 
+const app = express();
 const PORT = 3000;
+// in general these should be in a .env file,
+// but they're fine here for now since they're just localhost
+const url = 'mongodb://localhost:27017';
+const dbName = 'swapi'
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/planets', async (req, res) => {
+    try {
+        // Console log the entire request object
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('planets');
+        const planets = await collection.find({}).toArray();
+        res.json(planets);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("In space, no one can hear your API request... No planets found!");
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
